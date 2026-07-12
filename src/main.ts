ed6224cb5c07359f64dc6d924242eacb1d9f6dc4
zeoce <81248086+zeoce@users.noreply.github.com>
2026-07-11 20:41:59 -0700

    Streamline navigation and vehicle cards (#30)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { Application } from "./app/application";
 import { BRHeatMapPage } from "./app/page/br-heatmap-page";
 import { StackedAreaPage } from "./app/page/stacked-area-page";
-import { TodoPage } from "./app/page/todo-page";
 import { WebRepo } from "./app/link/web-repo";
 import { DataRepo } from "./app/link/data-repo";
 import { Logo } from "./app/image/logo";
-import { GithubIssue } from "./app/link/github-issue";
-import { Forum } from "./app/link/forum";
 import { DarkModeToggle } from "./app/link/dark-mode-toggle";
 
 Application.build
     .withLogo(Logo)
-    .withPages(BRHeatMapPage, StackedAreaPage, TodoPage)
-    .withLinks(WebRepo, DataRepo, GithubIssue, Forum, DarkModeToggle)
+    .withPages(BRHeatMapPage, StackedAreaPage)
+    .withLinks(WebRepo, DataRepo, DarkModeToggle)
     .class
     .run()
